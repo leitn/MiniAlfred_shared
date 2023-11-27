53332343b6331acb1e85f5cabd17ebbe1b2da9a6
leitn <leila.tnitan.pro@gmail.com>
2023-11-27 17:34:52 +0100

    fixed

--- srcs/builtin/bltn_pwd.c
+++ srcs/builtin/bltn_pwd.c
@@ -6,7 +6,7 @@
 /*   By: letnitan <letnitan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 18:26:17 by hedubois          #+#    #+#             */
-/*   Updated: 2023/11/06 14:55:28 by letnitan         ###   ########.fr       */
+/*   Updated: 2023/11/27 17:34:34 by letnitan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,16 @@ bool	ft_pwd(t_shell *shell, t_elem *cur, int pid)
 		return (false);
 	}
 	pwd = getcwd(NULL, 0);
-	ft_putstr_fd(pwd, cur->fd_wr);
-	ft_putchar_fd('\n', cur->fd_wr);
+	if (cur->fd_wr > 0 && (pid != 0))
+	{
+		ft_putstr_fd(pwd, cur->fd_wr);
+		ft_putchar_fd('\n', cur->fd_wr);
+	}
+	else if ((pid == 0) || (cur->fd_wr == -2))
+	{
+		ft_putstr_fd(pwd, 1);
+		ft_putchar_fd('\n', 1);
+	}
 	ft_add_to_the_bin(pwd, STR, shell->bin);
 	shell->exec_current = cur->next;
 	if (pid == 0)
